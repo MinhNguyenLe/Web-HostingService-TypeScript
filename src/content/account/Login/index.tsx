d778ae6900af510d3dbf040ddf6eb07fa105d105
PHUONGNAM011000 <namhungvuong00@gmail.com>
2021-09-09 20:13:09 +0700

    login update

--- src/content/account/Login/index.tsx
+++ src/content/account/Login/index.tsx
@@ -1,5 +1,217 @@
-function Login() {
-  return <div className="">Login page</div>;
-}
+import React, { useReducer, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import { makeStyles } from '@material-ui/styles';
+
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+// import CardHeader from '@material-ui/core/CardHeader';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    width: 400,
+    margin: '8px auto',
+    padding: '0 15px',
+  },
+  loginBtn: {
+    marginTop: '16px',
+    flexGrow: 1,
+    backgroundColor: '#8c7cf0 !important',
+    color: '#ffff !important',
+    '&:hover': {
+      backgroundColor: 'rgb(112, 99, 192) !important',
+    },
+  },
+  header: {
+    textAlign: 'center',
+    color: '#fff',
+    fontSize: '35px',
+    margin: '20px',
+  },
+  card: {
+    marginTop: '80px',
+  },
+  title: {
+    textAlign: 'center',
+    marginTop: '20px',
+  },
+});
+
+//state type
+
+type State = {
+  username: string;
+  password: string;
+  isButtonDisabled: boolean;
+  helperText: string;
+  isError: boolean;
+};
+
+const initialState: State = {
+  username: '',
+  password: '',
+  isButtonDisabled: true,
+  helperText: '',
+  isError: false,
+};
+
+type Action =
+  | { type: 'setUsername'; payload: string }
+  | { type: 'setPassword'; payload: string }
+  | { type: 'setIsButtonDisabled'; payload: boolean }
+  | { type: 'loginSuccess'; payload: string }
+  | { type: 'loginFailed'; payload: string }
+  | { type: 'setIsError'; payload: boolean };
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'setUsername':
+      return {
+        ...state,
+        username: action.payload,
+      };
+    case 'setPassword':
+      return {
+        ...state,
+        password: action.payload,
+      };
+    case 'setIsButtonDisabled':
+      return {
+        ...state,
+        isButtonDisabled: action.payload,
+      };
+    case 'loginSuccess':
+      return {
+        ...state,
+        helperText: action.payload,
+        isError: false,
+      };
+    case 'loginFailed':
+      return {
+        ...state,
+        helperText: action.payload,
+        isError: true,
+      };
+    case 'setIsError':
+      return {
+        ...state,
+        isError: action.payload,
+      };
+  }
+};
+
+const Login = () => {
+  const classes = useStyles();
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    if (state.username.trim() && state.password.trim()) {
+      dispatch({
+        type: 'setIsButtonDisabled',
+        payload: false,
+      });
+    } else {
+      dispatch({
+        type: 'setIsButtonDisabled',
+        payload: true,
+      });
+    }
+  }, [state.username, state.password]);
+
+  const handleLogin = () => {
+    if (state.username === '123@gmail.com' && state.password === '123') {
+      dispatch({
+        type: 'loginSuccess',
+        payload: 'Login Successfully',
+      });
+    } else {
+      dispatch({
+        type: 'loginFailed',
+        payload: 'Incorrect username or password',
+      });
+    }
+  };
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    if (event.keyCode === 13 || event.which === 13) {
+      state.isButtonDisabled || handleLogin();
+    }
+  };
+
+  const handleUsernameChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    dispatch({
+      type: 'setUsername',
+      payload: event.target.value,
+    });
+  };
+
+  const handlePasswordChange: React.ChangeEventHandler<HTMLInputElement> = (
+    event
+  ) => {
+    dispatch({
+      type: 'setPassword',
+      payload: event.target.value,
+    });
+  };
+  return (
+    <>
+      <form className={classes.container} noValidate autoComplete="off">
+        <Card className={classes.card}>
+          <p className={classes.header}>Login</p>
+          <CardContent>
+            <div>
+              <TextField
+                error={state.isError}
+                fullWidth
+                id="username"
+                type="email"
+                label="Username"
+                placeholder="Username"
+                margin="normal"
+                onChange={handleUsernameChange}
+                onKeyPress={handleKeyPress}
+              />
+              <TextField
+                error={state.isError}
+                fullWidth
+                id="password"
+                type="password"
+                label="Password"
+                placeholder="Password"
+                margin="normal"
+                helperText={state.helperText}
+                onChange={handlePasswordChange}
+                onKeyPress={handleKeyPress}
+              />
+            </div>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="contained"
+              size="large"
+              color="secondary"
+              className={classes.loginBtn}
+              onClick={handleLogin}
+              disabled={state.isButtonDisabled}
+            >
+              Login
+            </Button>
+          </CardActions>
+        </Card>
+      </form>
+      <div className={classes.title}>
+        Bạn chưa có tài khoản ?&nbsp;
+        <Link to="/account/Register">Đăng ký</Link>
+      </div>
+    </>
+  );
+};
 
 export default Login;
