10c79d664029144a22afdf6f6ca7e4881192bf7f
PHUONGNAM011000 <namhungvuong00@gmail.com>
2021-09-09 16:23:13 +0700

    update lan 1

---
+++ src/content/account/Register/index.tsx
@@ -0,0 +1,5 @@
+function Register() {
+  return <div className="">Register page</div>;
+}
+
+export default Register;
--- src/layouts/SidebarLayout/Sidebar/SidebarMenu/items.ts
+++ src/layouts/SidebarLayout/Sidebar/SidebarMenu/items.ts
@@ -1,20 +1,20 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
-import DesignServicesTwoToneIcon from '@material-ui/icons/DesignServicesTwoTone';
-import BrightnessLowTwoToneIcon from '@material-ui/icons/BrightnessLowTwoTone';
-import MmsTwoToneIcon from '@material-ui/icons/MmsTwoTone';
-import TableChartTwoToneIcon from '@material-ui/icons/TableChartTwoTone';
-import AccountCircleTwoToneIcon from '@material-ui/icons/AccountCircleTwoTone';
-import BallotTwoToneIcon from '@material-ui/icons/BallotTwoTone';
-import BeachAccessTwoToneIcon from '@material-ui/icons/BeachAccessTwoTone';
-import EmojiEventsTwoToneIcon from '@material-ui/icons/EmojiEventsTwoTone';
-import FilterVintageTwoToneIcon from '@material-ui/icons/FilterVintageTwoTone';
-import HowToVoteTwoToneIcon from '@material-ui/icons/HowToVoteTwoTone';
-import LocalPharmacyTwoToneIcon from '@material-ui/icons/LocalPharmacyTwoTone';
-import RedeemTwoToneIcon from '@material-ui/icons/RedeemTwoTone';
-import SettingsTwoToneIcon from '@material-ui/icons/SettingsTwoTone';
-import TrafficTwoToneIcon from '@material-ui/icons/TrafficTwoTone';
-import VerifiedUserTwoToneIcon from '@material-ui/icons/VerifiedUserTwoTone';
+import DesignServicesTwoToneIcon from "@material-ui/icons/DesignServicesTwoTone";
+import BrightnessLowTwoToneIcon from "@material-ui/icons/BrightnessLowTwoTone";
+import MmsTwoToneIcon from "@material-ui/icons/MmsTwoTone";
+import TableChartTwoToneIcon from "@material-ui/icons/TableChartTwoTone";
+import AccountCircleTwoToneIcon from "@material-ui/icons/AccountCircleTwoTone";
+import BallotTwoToneIcon from "@material-ui/icons/BallotTwoTone";
+import BeachAccessTwoToneIcon from "@material-ui/icons/BeachAccessTwoTone";
+import EmojiEventsTwoToneIcon from "@material-ui/icons/EmojiEventsTwoTone";
+import FilterVintageTwoToneIcon from "@material-ui/icons/FilterVintageTwoTone";
+import HowToVoteTwoToneIcon from "@material-ui/icons/HowToVoteTwoTone";
+import LocalPharmacyTwoToneIcon from "@material-ui/icons/LocalPharmacyTwoTone";
+import RedeemTwoToneIcon from "@material-ui/icons/RedeemTwoTone";
+import SettingsTwoToneIcon from "@material-ui/icons/SettingsTwoTone";
+import TrafficTwoToneIcon from "@material-ui/icons/TrafficTwoTone";
+import VerifiedUserTwoToneIcon from "@material-ui/icons/VerifiedUserTwoTone";
 
 export interface MenuItem {
   link?: string;
@@ -31,133 +31,148 @@ export interface MenuItems {
 
 const menuItems: MenuItems[] = [
   {
-    heading: '',
+    heading: "",
     items: [
       {
-        name: 'Overview',
-        link: '/overview',
-        icon: DesignServicesTwoToneIcon
-      }
-    ]
+        name: "Overview",
+        link: "/overview",
+        icon: DesignServicesTwoToneIcon,
+      },
+    ],
   },
   {
-    heading: 'Dashboards',
+    heading: "account",
     items: [
       {
-        name: 'Crypto',
-        link: '/dashboards/crypto',
-        icon: BrightnessLowTwoToneIcon
+        name: "Login",
+        link: "/account/login",
+        icon: DesignServicesTwoToneIcon,
       },
       {
-        name: 'Messenger',
+        name: "Register",
+        link: "/account/register",
+        icon: DesignServicesTwoToneIcon,
+      },
+    ],
+  },
+  {
+    heading: "Dashboards",
+    items: [
+      {
+        name: "Crypto",
+        link: "/dashboards/crypto",
+        icon: BrightnessLowTwoToneIcon,
+      },
+      {
+        name: "Messenger",
         icon: MmsTwoToneIcon,
-        link: '/dashboards/messenger'
+        link: "/dashboards/messenger",
       },
-    ]
+    ],
   },
   {
-    heading: 'Management',
+    heading: "Management",
     items: [
       {
-        name: 'Transactions',
+        name: "Transactions",
         icon: TableChartTwoToneIcon,
-        link: '/management/transactions'
+        link: "/management/transactions",
       },
       {
-        name: 'User Profile',
+        name: "User Profile",
         icon: AccountCircleTwoToneIcon,
-        link: '/management/profile',
+        link: "/management/profile",
         items: [
           {
-            name: 'Profile Details',
-            link: '/management/profile/details'
+            name: "Profile Details",
+            link: "/management/profile/details",
           },
           {
-            name: 'User Settings',
-            link: '/management/profile/settings'
-          }
-        ]
-      }
-    ]
+            name: "User Settings",
+            link: "/management/profile/settings",
+          },
+        ],
+      },
+    ],
   },
   {
-    heading: 'Components',
+    heading: "Components",
     items: [
       {
-        name: 'Buttons',
+        name: "Buttons",
         icon: BallotTwoToneIcon,
-        link: '/components/buttons'
+        link: "/components/buttons",
       },
       {
-        name: 'Modals',
+        name: "Modals",
         icon: BeachAccessTwoToneIcon,
-        link: '/components/modals'
+        link: "/components/modals",
       },
       {
-        name: 'Accordions',
+        name: "Accordions",
         icon: EmojiEventsTwoToneIcon,
-        link: '/components/accordions'
+        link: "/components/accordions",
       },
       {
-        name: 'Tabs',
+        name: "Tabs",
         icon: FilterVintageTwoToneIcon,
-        link: '/components/tabs'
+        link: "/components/tabs",
       },
       {
-        name: 'Badges',
+        name: "Badges",
         icon: HowToVoteTwoToneIcon,
-        link: '/components/badges'
+        link: "/components/badges",
       },
       {
-        name: 'Tooltips',
+        name: "Tooltips",
         icon: LocalPharmacyTwoToneIcon,
-        link: '/components/tooltips'
+        link: "/components/tooltips",
       },
       {
-        name: 'Avatars',
+        name: "Avatars",
         icon: RedeemTwoToneIcon,
-        link: '/components/avatars'
+        link: "/components/avatars",
       },
       {
-        name: 'Cards',
+        name: "Cards",
         icon: SettingsTwoToneIcon,
-        link: '/components/cards'
+        link: "/components/cards",
       },
       {
-        name: 'Forms',
+        name: "Forms",
         icon: TrafficTwoToneIcon,
-        link: '/components/forms'
+        link: "/components/forms",
       },
-    ]
+    ],
   },
   {
-    heading: 'Extra Pages',
+    heading: "Extra Pages",
     items: [
       {
-        name: 'Status',
+        name: "Status",
         icon: VerifiedUserTwoToneIcon,
-        link: '/status',
+        link: "/status",
         items: [
           {
-            name: 'Error 404',
-            link: '/status/404'
+            name: "Error 404",
+            link: "/status/404",
           },
           {
-            name: 'Error 500',
-            link: '/status/500'
+            name: "Error 500",
+            link: "/status/500",
           },
           {
-            name: 'Maintenance',
-            link: '/status/maintenance'
+            name: "Maintenance",
+            link: "/status/maintenance",
           },
           {
-            name: 'Coming Soon',
-            link: '/status/coming-soon'
-          }
-        ]
-      }
-    ]
-  }
+            name: "Coming Soon",
+            link: "/status/coming-soon",
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 export default menuItems;
