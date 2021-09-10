eb3c3da38abec0297986e167c91c3bcd0c4eef76
PHUONGNAM011000 <namhungvuong00@gmail.com>
2021-09-10 16:38:37 +0700

    fix form

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,17 +1,16 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import login from './en/login.json';
-// import register from './en/register.json';
+// import login from './en/login.json';
+import register from './en/register.json';
 
 export const resources = {
   en: {
-    login,
-    // register,
+    register,
   },
 } as const;
 
 i18n.use(initReactI18next).init({
   lng: 'en',
-  ns: ['login'],
+  ns: ['register'],
   resources,
 });
