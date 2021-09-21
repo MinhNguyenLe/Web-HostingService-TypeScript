24310488e80b1fb925e8f3218bd2712b386f64df
MinhNguyenLe <minh.nguyenle1809@gmail.com>
2021-09-21 19:03:00 +0700

    add multi api

--- src/components/DialogDomain/index.tsx
+++ src/components/DialogDomain/index.tsx
@@ -19,14 +19,32 @@ const Input = experimentalStyled("input")({
   display: "none",
 });
 
-const DialogDomain = ({ setItem, open, setOpen, createNew }) => {
+const DialogDomain = ({ item, setItem, open, setOpen, createNew }) => {
   const { t } = useTranslation("dialog");
 
+  const changeInfo = (type: string, value: string) => {
+    if (type === "price") {
+      setItem({ ...item, price: parseInt(value) });
+    }
+    if (type === "name") {
+      setItem({ ...item, name: value });
+    }
+    if (type === "information") {
+      setItem({ ...item, information: value });
+    }
+    if (type === "expiry") {
+      setItem({ ...item, expiry: parseInt(value) });
+    }
+  };
   const imgRef = useRef<HTMLInputElement>(null);
+  const inforRef = useRef<HTMLInputElement>(null);
+  const priceRef = useRef<HTMLInputElement>(null);
+  const expiryRef = useRef<HTMLInputElement>(null);
+  const nameRef = useRef<HTMLInputElement>(null);
   return (
     <div>
       <Dialog open={open} onClose={() => setOpen(false)}>
-        <DialogTitle sx={{ minWidth: 280, width: "360px" }}>
+        <DialogTitle sx={{ fontSize: "20px", minWidth: 280, width: "360px" }}>
           {t("1")}
         </DialogTitle>
         <DialogContent>
@@ -35,6 +53,8 @@ const DialogDomain = ({ setItem, open, setOpen, createNew }) => {
             We will send updates occasionally.
           </DialogContentText> */}
           <TextField
+            inputRef={priceRef}
+            onChange={() => changeInfo("price", priceRef.current.value)}
             autoFocus
             margin="dense"
             label={t("4")}
@@ -43,6 +63,8 @@ const DialogDomain = ({ setItem, open, setOpen, createNew }) => {
             variant="standard"
           />
           <TextField
+            inputRef={nameRef}
+            onChange={() => changeInfo("name", nameRef.current.value)}
             autoFocus
             margin="dense"
             label={t("5")}
@@ -50,18 +72,39 @@ const DialogDomain = ({ setItem, open, setOpen, createNew }) => {
             fullWidth
             variant="standard"
           />
+          <TextField
+            inputRef={inforRef}
+            onChange={() => changeInfo("information", inforRef.current.value)}
+            autoFocus
+            margin="dense"
+            label={t("7")}
+            type="text"
+            fullWidth
+            variant="standard"
+          />
+          <TextField
+            inputRef={expiryRef}
+            onChange={() => changeInfo("expiry", expiryRef.current.value)}
+            autoFocus
+            margin="dense"
+            label={t("8")}
+            type="text"
+            fullWidth
+            variant="standard"
+          />
           <Input
             ref={imgRef}
             onChange={() => {
-              setItem({ file: imgRef.current.files[0] });
+              setItem({ ...item, file: imgRef.current.files[0] });
             }}
             accept="image/*"
             id="change-cover"
             multiple
             type="file"
           />
-          <label htmlFor="change-cover" style={{ marginTop: "20px" }}>
+          <label htmlFor="change-cover">
             <Button
+              style={{ marginTop: "20px" }}
               startIcon={<UploadTwoToneIcon />}
               variant="contained"
               component="span"
--- src/content/admin/AddProduct/AddDomain.tsx
+++ src/content/admin/AddProduct/AddDomain.tsx
@@ -1,5 +1,5 @@
 // require("dotenv").config();
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   Grid,
@@ -28,6 +28,8 @@ import CardDomain from "../../../components/CardDomain";
 import DialogDomain from "../../../components/DialogDomain";
 
 import axios from "axios";
+
+import { gql, useMutation, useQuery } from "@apollo/client";
 
 const CardAddAction = experimentalStyled(Card)(
   ({ theme }) => `
@@ -65,8 +67,30 @@ const AvatarAddWrapper = experimentalStyled(Avatar)(
 function AddDomain() {
   const { t } = useTranslation(["addproduct"]);
 
+  const [dataDomain, setDataDomain] = useState();
+  // const {
+  //   loading: loadingDomain,
+  //   error: errorDomain,
+  //   data: dataDomain,
+  // } = useQuery(DOMAINS);
+  const listDomain = useQuery(DOMAINS);
+
+  useEffect(() => {
+    if (listDomain.loading) {
+      console.log("Loading list of list Domain");
+    } else {
+      setDataDomain(listDomain.data);
+      console.log(listDomain.data);
+    }
+  }, []);
+
   const [item, setItem] = useState({
     file: null,
+    name: "",
+    price: 0,
+    expiry: 0,
+    information: "",
+    images: "",
   });
   const [open, setOpen] = useState<boolean>(false);
 
@@ -74,13 +98,36 @@ function AddDomain() {
     setOpen(true);
   };
 
+  const [createDomain, { data, loading, error }] = useMutation(CREATE_DOMAIN, {
+    update(proxy, result) {
+      let data = result?.data?.createDomain;
+      console.log(data);
+    },
+    variables: {
+      images: item?.images,
+      dot: item?.name,
+      price: item?.price,
+      months: item?.expiry,
+      information: item?.information,
+    },
+  });
+
+  if (loading) console.log("loading");
+  if (error) console.log(JSON.stringify(error, null, 2));
+
   const createNew = () => {
+    console.log(item);
+
     const formData = new FormData();
     formData.append("file", item?.file);
     formData.append("upload_preset", "leminh2k");
-    axios.post(process.env.API_CLOUDINARY, formData).then((res) => {
-      console.log(res?.data?.secure_url);
-    });
+    axios
+      .post("https://api.cloudinary.com/v1_1/djes0pztf/image/upload", formData)
+      .then((res) => {
+        setItem({ ...item, images: res?.data?.secure_url });
+        createDomain();
+        console.log(item);
+      });
     setOpen(false);
   };
 
@@ -91,6 +138,7 @@ function AddDomain() {
       sx={{ paddingLeft: "24px", display: "flex", flexWrap: "wrap" }}
     >
       <DialogDomain
+        item={item}
         setItem={setItem}
         open={open}
         setOpen={setOpen}
@@ -129,5 +177,49 @@ function AddDomain() {
     </Grid>
   );
 }
+
+const CREATE_DOMAIN = gql`
+  mutation createDomain(
+    $dot: String!
+    $information: String!
+    $price: Float!
+    $months: Int!
+    $images: String!
+  ) {
+    createDomain(
+      createDomain: {
+        dot: $dot
+        information: $information
+        price: $price
+        months: $months
+        images: $images
+      }
+    ) {
+      _id
+      dot
+      information
+      images
+      product {
+        _id
+        months
+        price
+      }
+    }
+  }
+`;
+
+const DOMAINS = gql`
+  query domains {
+    _id
+    dot
+    information
+    images
+    product {
+      _id
+      months
+      price
+    }
+  }
+`;
 
 export default AddDomain;
